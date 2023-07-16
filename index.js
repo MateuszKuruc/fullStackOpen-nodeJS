7a2b94a558c3ff019d540220c016816025b513eb
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-07-16 12:24:06 +0200

    add production build of the frontend

--- index.js
+++ index.js
@@ -1,66 +1,70 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-
-morgan.token('body', req => JSON.stringify(req.body))
-
+const cors = require('cors');
+app.use(cors());
 app.use(express.json());
+app.use(express.static('build'));
+// morgan.token('body', req => JSON.stringify(req.body))
+
+
 
 // app.use(morgan("tiny"));
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+// app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+let notes = [
+    {
+      "id": 1,
+      "content": "HTML is easy",
+      "important": true
+    },
+    {
+      "id": 2,
+      "content": "Browser can execute only JavaScript",
+      "important": true
+    },
+    {
+      "id": 3,
+      "content": "GET and POST are the most important methods of HTTP protocol",
+      "important": false
+    },
+    {
+      "content": "mati",
+      "important": false,
+      "id": 4
+    }
+  ]
+
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
 
-app.get("/api/persons", (request, response) => {
-  response.json(persons);
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
 });
 
 console.log(new Date().toString());
 
 app.get("/info", (request, response) => {
-  const infoMessage = `Phonebook has info for ${persons.length} people`;
+  const infoMessage = `Phonebook has info for ${notes.length} people`;
   const timeInfo = new Date().toString();
   response.send(`<p>${infoMessage}</p><p>${timeInfo}</p>`);
 });
 
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/notes/:id", (request, response) => {
   const id = Number(request.params.id);
-  const person = persons.find((person) => person.id === id);
-  if (person) {
-    response.json(person);
+  const note = notes.find((note) => note.id === id);
+  if (note) {
+    response.json(note);
   } else {
     response.status(404).end();
   }
 });
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/notes/:id", (request, response) => {
   const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
+  notes = notes.filter((note) => note.id !== id);
   response.status(204).end();
 });
 
@@ -69,30 +73,32 @@ const generateId = () => {
   return id;
 };
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/notes", (request, response) => {
   const body = request.body;
 
-  if (!body.name || !body.number) {
+  if (!body.content || !body.important) {
     return response.status(400).json({
       error: "content missing",
     });
   }
-  if (persons.some((person) => person.name === body.name)) {
+  if (notes.some((note) => note.content === body.content)) {
     return response.status(400).json({
-      error: `${body.name} already exists`,
+      error: `${body.content} already exists`,
     });
   }
 
-  const person = {
+  const note = {
     id: generateId(),
-    name: body.name,
-    number: body.number,
+    content: body.content,
+    important: body.important,
   };
 
-  persons = persons.concat(person);
-  response.json(person);
+  notes = notes.concat(note);
+  response.json(note);
 });
 
-const PORT = 3001;
-app.listen(PORT);
-console.log(`Server running on port ${PORT}`);
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+})
+
