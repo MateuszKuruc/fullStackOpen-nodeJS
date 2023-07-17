e30d814ab01918ba1ec097bee8ae2c35e302fe18
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-07-17 23:06:37 +0200

    ex 3.15 done, delete handling

--- backend-phonebook/index.js
+++ backend-phonebook/index.js
@@ -1,54 +1,54 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const dotenv = require('dotenv');
+const cors = require("cors");
+const dotenv = require("dotenv");
 dotenv.config();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('build'));
+app.use(express.static("build"));
 
-const Person = require('./models/person');
+const Person = require("./models/person");
 
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World</h1');
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World</h1");
 });
 
-app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
-      response.json(persons)
+app.get("/api/persons", (request, response) => {
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
+});
+
+app.post("/api/persons", (request, response) => {
+  const body = request.body;
+  console.log("request body", request.body);
+
+  if (body.name === undefined || body.number === undefined) {
+    return response.status(400).json({
+      error: "name or number is missing",
+    });
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  });
+
+  person.save().then((savedPerson) => {
+    response.json(savedPerson);
+  });
+});
+
+app.delete("/api/persons/:id", (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
     })
-})
-
-app.post('/api/persons', (request, response) => {
-    const body = request.body;
-    console.log('request body', request.body);
-
-    if (body.name === undefined || body.number === undefined) {
-        return response.status(400).json({
-            error: 'name or number is missing'
-        })
-    }
-
-    const person = new Person({
-        name: body.name,
-        number: body.number
-    })
-
-    person.save().then(savedPerson => {
-        response.json(savedPerson)
-    })
-})
-
-app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findByIdAndRemove(request.params.id)
-    .then(result => {
-        response.status(204).end()
-    })
-    .catch(error => next(error))
-})
+    .catch((error) => next(error));
+});
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-})
+  console.log(`Server running on port ${PORT}`);
+});
