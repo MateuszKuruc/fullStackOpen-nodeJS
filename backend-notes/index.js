7230acc301d6878c979761fd846eec2c782172b2
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-07-17 22:45:38 +0200

    add delete handler for notes

--- backend-notes/index.js
+++ backend-notes/index.js
@@ -45,16 +45,24 @@ app.get('/api/notes/:id', (request, response, next) => {
   })
 })
 
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
+})
+
 app.delete("/api/notes/:id", (request, response) => {
   const id = Number(request.params.id);
   notes = notes.filter((note) => note.id !== id);
   response.status(204).end();
 });
 
-const generateId = () => {
-  const id = Math.floor(Math.random() * 999999 + 1);
-  return id;
-};
+// const generateId = () => {
+//   const id = Math.floor(Math.random() * 999999 + 1);
+//   return id;
+// };
 
 app.post("/api/notes", (request, response) => {
   const body = request.body;
