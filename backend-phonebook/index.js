cbb0e6224e266e272905efb61b8b03741921df10
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-07-17 18:57:47 +0200

    ex 3.13 done, data now fetched from database, mongoose moved to module

--- backend-phonebook/index.js
+++ backend-phonebook/index.js
@@ -1,40 +1,26 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static('build'));
 
-console.log('bububu');
-
-let persons = [
-      {
-        "name": "mati",
-        "number": "888",
-        "id": 1
-      },
-      {
-        "name": "Puszkinson Crusoe",
-        "number": "666",
-        "id": 2
-      },
-      {
-        "name": "rabarbar",
-        "number": "777",
-        "id": 3
-      }
-    ]
+const Person = require('./models/person');
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World</h1');
 });
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+      response.json(persons)
+    })
 })
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
