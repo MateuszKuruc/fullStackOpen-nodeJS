1a9f7616dcbe1237fc2bce950a1fddd33f4ce424
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-07-18 14:25:28 +0200

    ex 3.22 done, add eslint, define rules and fix errors

--- backend-notes/models/note.js
+++ backend-notes/models/note.js
@@ -1,36 +1,37 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 dotenv.config();
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 
-// const password = process.env.PASSWORD;
 const url = process.env.MONGODB_URI;
 
-console.log('connecting to', url);
+console.log("connecting to", url);
 
-mongoose.connect(url)
-.then(result => {
-    console.log('connected to MongoDB');
-})
-.catch((error) => {
-    console.log('error connecting to MongoDB:', error.message);
-})
+mongoose
+  .connect(url)
+  // eslint-disable-next-line no-unused-vars
+  .then((result) => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
 
 const noteSchema = new mongoose.Schema({
-    content: {
-      type: String,
-      minLength: 5,
-      required: true
-    },
-    important: Boolean
-  })
+  content: {
+    type: String,
+    minLength: 5,
+    required: true,
+  },
+  important: Boolean,
+});
 
-  noteSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString();
-      delete returnedObject._id;
-      delete returnedObject.__v;
-    }
-  })
+noteSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
-module.exports = mongoose.model('Note', noteSchema);
+module.exports = mongoose.model("Note", noteSchema);
