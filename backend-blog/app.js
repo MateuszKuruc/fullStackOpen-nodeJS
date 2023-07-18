190c6c62603378b7741186d24d3a283474751e6e
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-07-19 00:04:57 +0200

    ex 4.2 done, improve structure of the directory, divide and clean code

---
+++ backend-blog/app.js
@@ -0,0 +1,25 @@
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
+const blogsRouter = require("./controllers/blogs");
+
+const mongoUrl = config.MONGODB_URI;
+
+mongoose
+  .connect(mongoUrl)
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connecting to MongoDb:", error.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/blogs", blogsRouter);
+
+module.exports = app;
--- backend-blog/utils/config.js
+++ backend-blog/utils/config.js
@@ -1,9 +1,10 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const PORT = 3003;
-const MONGODB_URI='mongodb+srv://mateuszkuruc:defenestracja12@cluster0.xuvl4j3.mongodb.net/blogApp?retryWrites=true&w=majority';
+const MONGODB_URI =
+  "mongodb+srv://mateuszkuruc:defenestracja12@cluster0.xuvl4j3.mongodb.net/blogApp?retryWrites=true&w=majority";
 
 module.exports = {
-    MONGODB_URI,
-    PORT
-}
+  MONGODB_URI,
+  PORT,
+};
