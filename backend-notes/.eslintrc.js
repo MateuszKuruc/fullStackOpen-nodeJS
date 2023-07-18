a466c911e08a58fbd661fd929516e3d0bc1da7b0
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-07-18 22:05:09 +0200

    change structure of the files in the backend directory

--- backend-notes/.eslintrc.js
+++ backend-notes/.eslintrc.js
@@ -34,4 +34,4 @@ module.exports = {
     ],
     "no-console": 0
   },
-};
+}
--- backend-notes/models/note.js
+++ backend-notes/models/note.js
@@ -1,21 +1,4 @@
 const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-dotenv.config();
-mongoose.set("strictQuery", false);
-
-const url = process.env.MONGODB_URI;
-
-console.log("connecting to", url);
-
-mongoose
-  .connect(url)
-  // eslint-disable-next-line no-unused-vars
-  .then((result) => {
-    console.log("connected to MongoDB");
-  })
-  .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
 
 const noteSchema = new mongoose.Schema({
   content: {
