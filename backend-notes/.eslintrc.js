9cf98424242fbaccc7b16cfd8105a7f430a4a20f
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-07-19 10:35:01 +0200

    add tests, install jest

--- backend-notes/.eslintrc.js
+++ backend-notes/.eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     node: true,
     commonjs: true,
     es2021: true,
+    jest: true,
   },
   extends: "eslint:recommended",
   overrides: [
@@ -26,12 +27,8 @@ module.exports = {
     semi: ["error", "always"],
     eqeqeq: "error",
     "no-trailing-spaces": "error",
-    "object-curly-spacing": [
-      "error", "always"
-    ],
-    "arrow-spacing": [
-      "error", { "before": true, "after": true }
-    ],
-    "no-console": 0
+    "object-curly-spacing": ["error", "always"],
+    "arrow-spacing": ["error", { before: true, after: true }],
+    "no-console": 0,
   },
-}
+};
