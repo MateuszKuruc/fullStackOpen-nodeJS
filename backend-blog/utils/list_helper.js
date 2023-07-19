cc7c6b0a37cf072d646f7db0c37220d1ea7f9f15
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-07-19 15:13:42 +0200

    ex 4.4 done, tests written for total like across multiple blogs

--- backend-blog/utils/list_helper.js
+++ backend-blog/utils/list_helper.js
@@ -2,6 +2,23 @@ const dummy = (blogs) => {
   return 1;
 };
 
+// const totalLikes = (blogs) => {
+//   const likes = blogs.map((blog) => blog.likes);
+
+//   const reducer = (total, number) => {
+//     return total + number
+//   }
+
+//   return likes.reduce(reducer, 0);
+// };
+
+const totalLikes = (blogs) => {
+    const likes = blogs.map(blog => blog.likes);
+
+    return likes.reduce((total, number) => total + number, 0)
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 };
