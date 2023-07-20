c11e0d5010c605d1f31bfa210a5b6987fb75a595
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-07-20 13:36:21 +0200

    start refactoring backend code with async and await functions

--- backend-notes/mongo.js
+++ backend-notes/mongo.js
@@ -1,51 +1,51 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 if (process.argv.length < 3) {
-    console.log('give password as argument');
-    process.exit(1);
+  console.log("give password as argument");
+  process.exit(1);
 }
 
-const password = process.argv[2]
+const password = process.argv[2];
 
-const url = `mongodb+srv://mateuszkuruc:${password}@cluster0.xuvl4j3.mongodb.net/noteApp?retryWrites=true&w=majority`
+// const url = `mongodb+srv://mateuszkuruc:${password}@cluster0.xuvl4j3.mongodb.net/testNoteApp?retryWrites=true&w=majority`
 
-mongoose.set('strictQuery', false);
-mongoose.connect(url);
+mongoose.set("strictQuery", false);
+mongoose.connect(process.env.TEST_MONGODB_URI);
 
 const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean,
-})
+  content: String,
+  important: Boolean,
+});
 
-const Note = mongoose.model('Note', noteSchema);
+const Note = mongoose.model("Note", noteSchema);
 
 const note = new Note({
-    content: 'HTML is easy',
-    important: true
-})
+  content: "HTML is easy",
+  important: true,
+});
 
 const note3 = new Note({
-    content: 'CSS is hard',
-    important: true
-})
+  content: "CSS is hard",
+  important: true,
+});
 
-const note2 = new Note({
-    content: 'Mongoose makes things easy',
-    important: true
-})
+// const note2 = new Note({
+//     content: 'Mongoose makes things easy',
+//     important: true
+// })
 
-note.save().then(result => {
-    console.log('note saved!');
-    mongoose.connection.close();
-})
+note.save().then((result) => {
+  console.log("note saved!");
+  mongoose.connection.close();
+});
 
+// note2.save().then(result => {
+//     console.log('note saved!');
+//     mongoose.connection.close();
+// })
 
-note2.save().then(result => {
-    console.log('note saved!');
-    mongoose.connection.close();
-})
-
-note3.save().then(result => {
-    console.log('note saved!');
-    mongoose.connection.close();
-})
+note3.save().then((result) => {
+  console.log("note saved!");
+  mongoose.connection.close();
+});
--- backend-notes/utils/config.js
+++ backend-notes/utils/config.js
@@ -5,8 +5,6 @@ const MONGODB_URI = process.env === 'test'
   ? process.env.TEST_MONGODB_URI
   : process.env.MONGODB_URI
 
-process.env.MONGODB_URI;
-
 module.exports = {
   MONGODB_URI,
   PORT,
