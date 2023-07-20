0470bfd374ceee6a2317366d9afbd622d2efec11
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-07-20 21:08:00 +0200

    add express async error library, adjust routers

--- backend-notes/controllers/notes.js
+++ backend-notes/controllers/notes.js
@@ -14,9 +14,7 @@ notesRouter.get("/:id", async (request, response, next) => {
     } else {
       response.status(404);
     }
-  } catch (exception) {
-    next(exception);
-  }
+  } 
 });
 
 notesRouter.post("/", async (request, response, next) => {
@@ -29,32 +27,28 @@ notesRouter.post("/", async (request, response, next) => {
   try {
     const savedNote = await note.save();
     response.status(201).json(savedNote);
-  } catch (exception) {
-    next(exception);
-  }
+  } 
 });
 
 notesRouter.delete("/:id", async (request, response, next) => {
   try {
     await Note.findByIdAndRemove(request.params.id);
     response.status(204).end();
-  } catch (exception) {
-    next(exception);
-  }
+  } 
 });
 
-notesRouter.put("/:id", (request, response, next) => {
-  const { content, important } = request.body;
+// notesRouter.put("/:id", (request, response, next) => {
+//   const { content, important } = request.body;
 
-  Note.findByIdAndUpdate(
-    request.params.id,
-    { content, important },
-    { new: true, runValidators: true, context: "query" }
-  )
-    .then((updatedNote) => {
-      response.json(updatedNote);
-    })
-    .catch((error) => next(error));
-});
+//   Note.findByIdAndUpdate(
+//     request.params.id,
+//     { content, important },
+//     { new: true, runValidators: true, context: "query" }
+//   )
+//     .then((updatedNote) => {
+//       response.json(updatedNote);
+//     })
+//     .catch((error) => next(error));
+// });
 
 module.exports = notesRouter;
