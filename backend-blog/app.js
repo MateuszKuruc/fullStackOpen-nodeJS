cdc4b362f646befbcc9c8b8f20a96f80b72b288f
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-07-21 11:36:11 +0200

    :

--- backend-blog/app.js
+++ backend-blog/app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const logger = require("./utils/logger");
 const mongoose = require("mongoose");
 const blogsRouter = require("./controllers/blogs");
+require("express-async-errors");
 
 const mongoUrl = config.MONGODB_URI;
 
