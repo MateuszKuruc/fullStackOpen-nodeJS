6d05b2fd0b4c90332ac32ee64ba6cc1ff7c38576
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-07-22 14:10:51 +0200

    ex 4.13 done, add functionality for deleting individual blog and write tests for it

--- backend-blog/tests/blog_api.test.js
+++ backend-blog/tests/blog_api.test.js
@@ -82,6 +82,20 @@ test.skip("if title or url is missing, show 400 error", async () => {
   await api.post("/api/blogs").send(newBlog2).expect(400);
 }, 100000);
 
+test("check if blog post is deleted", async () => {
+  const blogsAtStart = await helper.blogsInDb();
+  const blogToDelete = blogsAtStart[0];
+
+  await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
+
+  const blogsAtEnd = await helper.blogsInDb();
+
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1);
+  const contents = blogsAtEnd.map((blog) => blog.title);
+
+  expect(contents).not.toContain(blogToDelete.title);
+});
+
 afterAll(async () => {
   await mongoose.connection.close();
 });
