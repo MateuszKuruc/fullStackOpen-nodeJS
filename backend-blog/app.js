0d0f24f1ad906a64eeca406195bc70dac77cf619
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-07-24 13:44:58 +0200

    add users controller and connect it with app

--- backend-blog/app.js
+++ backend-blog/app.js
@@ -6,6 +6,7 @@ const logger = require("./utils/logger");
 const mongoose = require("mongoose");
 const blogsRouter = require("./controllers/blogs");
 require("express-async-errors");
+const usersRouter = require("./controllers/users");
 
 mongoose
   .connect(config.MONGODB_URI)
@@ -20,5 +21,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 
 module.exports = app;
