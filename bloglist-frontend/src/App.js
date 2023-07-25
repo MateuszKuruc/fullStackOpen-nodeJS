71d5284cdba5d7ca8cd968e7d676fde8c7ab0c7a
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-07-25 17:19:03 +0200

    add state control for username, password and user info

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -1,24 +1,42 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import blogService from "./services/blogs";
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [username, setUsername] = useState([]);
+  const [password, setPassword] = useState([]);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    console.log('logging in with', username, password);
+  }
 
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      <h1>Login</h1>
+      <form onSubmit={handleLogin}>
+    <div>
+      username
+      <input
+      type="text"
+      value={username}
+      name="Username"
+      onChange={({ target }) => setUsername(target.value)}
+      />
     </div>
-  )
-}
+      </form>
+      <h2>blogs</h2>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog} />
+      ))}
+    </div>
+  );
+};
 
-export default App
+export default App;
