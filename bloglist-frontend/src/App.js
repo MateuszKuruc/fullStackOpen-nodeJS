ea91e4c6a3d5862e344d48e9152a94d6ea527eb7
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-07-26 09:56:31 +0200

    ex 5.1 done, finish login form

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -32,40 +32,39 @@ const App = () => {
   const loginForm = () => (
     <form onSubmit={handleLogin}>
       <h1>Login</h1>
-        <div>
-          username
-          <input
-            type="text"
-            value={username}
-            name="Username"
-            onChange={({ target }) => setUsername(target.value)}
-          />
-        </div>
-        <div>
-          password
-          <input
-            onSubmit={handleLogin}
-            type="password"
-            value={password}
-            name="Password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-  )
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          onSubmit={handleLogin}
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
 
   const displayBlog = () => (
     <div>
-    <h2>blogs</h2>
-    
+      <h2>blogs</h2>
+
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
-      </div>
-  )
+    </div>
+  );
 
-  
   return (
     <div>
       {user === null && loginForm()}
