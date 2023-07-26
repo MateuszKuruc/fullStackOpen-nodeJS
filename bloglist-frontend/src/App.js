3db6caebce427e64ac608b2e3b9a1cfb4eaf5892
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-07-26 09:55:18 +0200

    add conditional functions to display either login form or blog posts depending on user status

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -29,7 +29,7 @@ const App = () => {
     }
   };
 
-  const loginForm = () => {
+  const loginForm = () => (
     <form onSubmit={handleLogin}>
       <h1>Login</h1>
         <div>
@@ -53,16 +53,23 @@ const App = () => {
         </div>
         <button type="submit">Login</button>
       </form>
-  }
+  )
 
-  return (
+  const displayBlog = () => (
     <div>
-      
-      
-      <h2>blogs</h2>
+    <h2>blogs</h2>
+    
       {blogs.map((blog) => (
         <Blog key={blog.id} blog={blog} />
       ))}
+      </div>
+  )
+
+  
+  return (
+    <div>
+      {user === null && loginForm()}
+      {user !== null && displayBlog()}
     </div>
   );
 };
