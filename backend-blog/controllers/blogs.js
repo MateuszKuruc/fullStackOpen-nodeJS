ac335fead8107022875361e9c3f9789cb58a99b3
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-07-28 00:54:18 +0200

    backend for ex 5.8 fixed with populate for post request

--- backend-blog/controllers/blogs.js
+++ backend-blog/controllers/blogs.js
@@ -30,6 +30,7 @@ blogsRouter.post("/", async (request, response) => {
   });
 
   const savedBlog = await blog.save();
+  savedBlog.populate("user");
   user.blogs = user.blogs.concat(savedBlog.id);
   await user.save();
 
@@ -45,9 +46,6 @@ blogsRouter.delete("/:id", async (request, response) => {
   }
   const user = await User.findById(decodedToken.id);
   const blog = await Blog.findById(id);
-  console.log("DELETE THIS SHIT YOOOOO", blog.user.toString());
-  console.log("user", user.id);
-  console.log(blog.user.toString() === user.id.toString());
 
   if (user.id.toString() !== blog.user.toString()) {
     return response.status(401).json({ error: "No authorization to do that" });
@@ -64,15 +62,17 @@ blogsRouter.delete("/:id", async (request, response) => {
 blogsRouter.put("/:id", async (request, response) => {
   const body = request.body;
   const blog = {
-    title: request.body.title,
-    author: request.body.author,
-    url: request.body.url,
-    likes: request.body.likes,
+    user: body.user,
+    id: body.id,
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
   };
 
   const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {
     new: true,
-  });
+  }).populate("user", { username: 1, name: 1 });
   response.json(updatedBlog);
 });
 
