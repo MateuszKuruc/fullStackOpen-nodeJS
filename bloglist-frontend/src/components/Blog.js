91a50b03a39d42945f651da912f3ecb0dfef40f9
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-07-27 14:14:48 +0200

    ex 5.7 done, add details display control in blog component based on toggle mechanic

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,21 +1,34 @@
+import { useState } from "react";
+
 const Blog = ({ blog }) => {
-const blogStyle = {
-  paddingTop: 10,
-  paddingLeft: 2,
-  border: 'solid',
-  borderWidth: 1,
-  marginBottom: 5
-}
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  const [details, setDetails] = useState(false);
+
+  const detailsShown = { display: details ? "" : "none" };
+  const toggleDetails = () => {
+    setDetails(!details);
+  };
 
   return (
     <div style={blogStyle}>
       <div>
-        {blog.title} {blog.author} 
-        <button>view</button>
+        {blog.title} {blog.author}
+        <button onClick={toggleDetails}>view</button>
       </div>
-      url: {blog.url}
-      likes: {blog.likes}
-      user: {blog.user.username} 
+      <div style={detailsShown}>
+        <div>url: {blog.url}</div>
+        <div>
+          likes: {blog.likes} <button>like</button>
+        </div>
+        <div>user: {blog.user.username}</div>
+      </div>
     </div>
   );
 };
