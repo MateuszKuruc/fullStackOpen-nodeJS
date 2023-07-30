cb535d8af14bc0fe95334e6be3a8ca26b06379e7
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-07-30 16:57:00 +0200

    write beforeeach function to login bypassing UI

--- bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -25,17 +25,32 @@ describe("Blog app", function () {
 
       cy.contains("matislav logged in");
     });
+
+    it("fails with wrong credentials", function () {
+      cy.contains("login").click();
+      cy.get("#username").type("mati");
+      cy.get("#password").type("wrong");
+      cy.get("#login-button").click();
+
+      cy.get(".error")
+        .should("contain", "Wrong credentials")
+        .and("have.css", "color", "rgb(255, 0, 0)")
+        .and("have.css", "border-style", "solid");
+    });
   });
-
-  it("fails with wrong credentials", function () {
-    cy.contains("login").click();
-    cy.get("#username").type("mati");
-    cy.get("#password").type("wrong");
-    cy.get("#login-button").click();
-
-    cy.get(".error")
-      .should("contain", "Wrong credentials")
-      .and("have.css", "color", "rgb(255, 0, 0)")
-      .and("have.css", "border-style", "solid");
+  describe("when logged in", function () {
+    beforeEach(function () {
+      cy.request("POST", "http://localhost:3003/api/login", {
+        username: "mati",
+        password: "mati123",
+      }).then((response) => {
+        localStorage.setItem(
+          "loggedNoteappUser",
+          JSON.stringify(response.body)
+        );
+        cy.visit("http://localhost:3000");
+      });
+    });
+    
   });
 });
