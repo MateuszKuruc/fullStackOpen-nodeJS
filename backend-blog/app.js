7d95812cad8754235ca7141b605208ef2aefa18d
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-07-30 14:34:33 +0200

    add testing router used to clear db of blogs and users for testing purposes

--- backend-blog/app.js
+++ backend-blog/app.js
@@ -25,6 +25,11 @@ app.use(express.json());
 app.use(middleware.tokenExtractor);
 app.use(middleware.errorHandler);
 
+if (process.env.NODE_ENV === "test") {
+  const testingRouter = require("./controllers/testing");
+  app.use("/api/testing", testingRouter);
+}
+
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter);
