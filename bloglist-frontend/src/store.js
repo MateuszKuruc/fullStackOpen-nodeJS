e877ab89b846ae5632250c0c0dbad03a43755d57
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-08-14 11:04:00 +0200

    add basis for blog reducer, adjust store

--- bloglist-frontend/src/store.js
+++ bloglist-frontend/src/store.js
@@ -1,11 +1,13 @@
 import messageReducer from "./reducers/messageReducer";
 import { configureStore } from "@reduxjs/toolkit";
 import errorMessageReducer from "./reducers/errorMessageReducer";
+import blogReducer from "./reducers/blogReducer";
 
 const store = configureStore({
   reducer: {
     message: messageReducer,
     errorMessage: errorMessageReducer,
+    blogs: blogReducer,
   },
 });
 
