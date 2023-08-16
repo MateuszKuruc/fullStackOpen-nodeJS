3466fb699ed44cf0bac8e14bc362bd64182769a2
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-08-16 20:51:39 +0200

    implement users reducer and adjust redux store

---
+++ bloglist-frontend/src/reducers/usersReducer.js
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const usersSlice = createSlice({
+  name: "users",
+  initialState: null,
+  reducers: {},
+});
+
+export default usersSlice.reducer;
--- bloglist-frontend/src/store.js
+++ bloglist-frontend/src/store.js
@@ -3,6 +3,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import errorMessageReducer from "./reducers/errorMessageReducer";
 import blogReducer from "./reducers/blogReducer";
 import loginReducer from "./reducers/loginReducer";
+import usersReducer from "./reducers/usersReducer";
 
 const store = configureStore({
   reducer: {
@@ -10,6 +11,7 @@ const store = configureStore({
     errorMessage: errorMessageReducer,
     blogs: blogReducer,
     login: loginReducer,
+    users: usersReducer,
   },
 });
 
