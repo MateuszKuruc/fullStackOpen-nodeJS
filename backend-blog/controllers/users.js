744933edb0f8a12649ec9678e504793c9c7d8daa
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-08-16 21:09:29 +0200

    adjust code so the users router shows the list of all the users created

--- backend-blog/controllers/users.js
+++ backend-blog/controllers/users.js
@@ -3,13 +3,15 @@ const usersRouter = require("express").Router();
 const User = require("../models/user");
 
 usersRouter.get("/", async (request, response) => {
-  const users = await User.find({}).populate("blog", {
-    author: 1,
-    title: 1,
-    url: 1,
-    likes: 1,
-  });
+  const users = await User.find({})
   response.json(users);
+
+  // .populate("blog", {
+  //   author: 1,
+  //   title: 1,
+  //   url: 1,
+  //   likes: 1,
+  // });
 });
 
 usersRouter.post("/", async (request, response) => {
