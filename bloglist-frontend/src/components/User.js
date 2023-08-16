9eb79cdf42217a855164a5618c0683702c417cf6
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-08-16 22:44:01 +0200

    ex 7.14 done, imlement view option to display basic info about all the users in db

--- bloglist-frontend/src/components/User.js
+++ bloglist-frontend/src/components/User.js
@@ -1,3 +1,20 @@
-const User = () => {};
+const User = ({ user }) => {
+  return (
+    <div>
+      <table>
+        <thead></thead>
+        <tbody>
+          <tr>
+            {/* <td>{user.username}</td> */}
+            <td>
+              <h4>{user.name}</h4>
+            </td>
+            <td>blogs created: {user.blogs.length}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
 export default User;
