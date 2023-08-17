8f5205d631e56d8a61d765fe4f38c3aac7c3a003
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-08-17 09:46:37 +0200

    clean code in Users and UserDetails components, refactor and remove unused elements

--- bloglist-frontend/src/components/UserDetails.js
+++ bloglist-frontend/src/components/UserDetails.js
@@ -3,43 +3,22 @@
 
 import { useParams } from "react-router-dom";
 
-// const UserDetails = ({ user }) => {
-//   const users = useSelector((state) => state.users);
-
-//   const id = useParams().id;
-//   console.log("id", id);
-//   const specificUser = users.find((user) => user.id === id);
-//   console.log("specificUser", specificUser);
-//   console.log("user props", user);
-
-//   if (!user) {
-//     return null;
-//   }
-
-//   return (
-//     <div>
-//       <h2>test 123</h2>
-//       {specificUser.username}
-//     </div>
-//   );
-
-//   //   return (
-//   //     <div>
-//   //       <ul key={user.id}>{user.name}</ul>
-//   //       {user.blogs.map((blog) => (
-//   //         <li key={blog.id}>{blog.title}</li>
-//   //       ))}
-//   //     </div>
-//   //   );
-// };
-
 const UserDetails = ({ users }) => {
   const id = useParams().id;
   console.log("id", id);
   const user = users.find((user) => user.id === id);
   console.log("user found", user);
 
-  return <div>{user.name}</div>;
+  return (
+    <div>
+      <h2>{user.name}</h2>
+      <ul>
+        {user.blogs.map((blog) => (
+          <li key={blog.id}>{blog.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default UserDetails;
--- bloglist-frontend/src/components/Users.js
+++ bloglist-frontend/src/components/Users.js
@@ -1,45 +1,15 @@
-// import { Link, Routes, Route } from "react-router-dom";
-// import UserDetails from "./UserDetails";
-
 import { Link } from "react-router-dom";
-
-// const User = ({ user }) => {
-//   console.log("user in user", user);
-//   return (
-//     <div>
-//       <table>
-//         <thead></thead>
-//         <tbody>
-//           <tr>
-//             <td>
-//               <Link to={`/users/${user.id}`}>
-//                 <h4>{user.name}</h4>
-//               </Link>
-//             </td>
-//             <td>blogs created: {user.blogs.length}</td>
-//           </tr>
-//         </tbody>
-//       </table>
-//       <div>
-//         <Routes>
-//           <Route path="/users/:id" element={<UserDetails user={user} />} />
-//         </Routes>
-//       </div>
-//     </div>
-//   );
-// };
 
 const Users = ({ users }) => {
   console.log("users", users);
   return (
     <div>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            <Link to={`/users/${user.id}`}>{user.username},</Link> blogs created: {user.blogs.length}
-          </li>
-        ))}
-      </ul>
+      {users.map((user) => (
+        <div key={user.id}>
+          <Link to={`/users/${user.id}`}>{user.username},</Link> blogs created:{" "}
+          {user.blogs.length}
+        </div>
+      ))}
     </div>
   );
 };
