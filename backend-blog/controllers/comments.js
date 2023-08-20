591fa0cfbd4fa73a311f18b77adcc538a81253b8
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-08-20 20:02:52 +0200

    add get request for comments router

--- backend-blog/controllers/comments.js
+++ backend-blog/controllers/comments.js
@@ -2,4 +2,18 @@ const commentsRouter = require("express").Router();
 const Comment = require("../models/comment");
 const Blog = require("../models/blog");
 
+commentsRouter.get("/:id/comments", async (request, response) => {
+  const comments = await Comment.find({}).populate("blog");
+  if (comments) {
+    response.json(comments);
+  } else {
+    response.status(404).end();
+  }
+});
+
+commentsRouter.post("/:id/comments", async (request, response) => {
+  const body = await request.body;
+  
+});
+
 module.exports = commentsRouter;
