2ba8e578a0223a9399297bf09da0914d4db14e27
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-08-21 23:55:06 +0200

    style users component to display all users names and number of their blogs in a table

--- bloglist-frontend/src/components/Users.js
+++ bloglist-frontend/src/components/Users.js
@@ -1,14 +1,37 @@
 import { Link } from "react-router-dom";
+import {
+  Table,
+  TableRow,
+  TableCell,
+  TableContainer,
+  TableBody,
+} from "@mui/material";
 
 const Users = ({ users }) => {
   return (
     <div>
-      {users.map((user) => (
-        <div key={user.id}>
-          <Link to={`/users/${user.id}`}>{user.username},</Link> blogs created:{" "}
-          {user.blogs.length}
-        </div>
-      ))}
+      <TableContainer>
+        <Table>
+          <TableBody>
+            <TableRow>
+              <TableCell>
+                <h3>User name</h3>
+              </TableCell>
+              <TableCell>
+                <h3>Number of blogs</h3>
+              </TableCell>
+            </TableRow>
+            {users.map((user) => (
+              <TableRow key={user.id}>
+                <TableCell>
+                  <Link to={`/users/${user.id}`}>{user.username}</Link>
+                </TableCell>
+                <TableCell>{user.blogs.length}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 };
