a89d04e275508707e805ead3122e5546a85d37d0
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2023-11-20 13:02:17 +0100

    clean console log statements for requests

--- bloglist-frontend/src/services/login.js
+++ bloglist-frontend/src/services/login.js
@@ -4,9 +4,8 @@ import axios from "axios";
 const baseUrl = "https://bloglist-backend-t9tb.onrender.com/api/login";
 
 const login = async (credentials) => {
-  console.log("credentials", credentials);
   const response = await axios.post(baseUrl, credentials);
-  console.log("login response data", response.data);
+
   return response.data;
 };
 
